b3b721134029915363248c8302a93a252703f475
Aditya Shinde <adityashinde4580@gmail.com>
2023-01-05 23:31:28 +0530

    Upload files

---
+++ Currency_Converter-Task_01/src/CurrencyConverter/CurrencyConverter.java
@@ -0,0 +1,28 @@
+package CurrencyConverter;
+
+import CurrencyConverter.MainWindow;
+import java.awt.EventQueue;
+import javax.swing.UIManager;
+
+public class CurrencyConverter {
+
+        public static void main(String[] args) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+
+            EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    try {
+                        // Create and show main window at startup
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.setVisible(true);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
